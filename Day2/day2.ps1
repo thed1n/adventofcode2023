1f51d0c756cbbcaa977cb56c5b5b9510c6eb65ba
thed1n <tomas.hedin@gmail.com>
2023-12-02 08:26:39 +0100

    day2

---
+++ Day2/day2.ps1
@@ -0,0 +1,39 @@
+$data = get-content .\input.txt
+
+$limits = [ordered]@{
+    red = 12
+    green = 13
+    blue = 14
+}
+[system.collections.generic.list[pscustomobject]]$part2result = @()
+$sumofids = $data| % {
+    $part2 = [ordered]@{
+        gameid = 0
+        red = 0
+        green = 0
+        blue = 0
+    }
+    [int]$gameid =$_ -replace 'Game (\d+):.+','$1'
+    $result = [PSCustomObject]@{
+        Game = $gameid
+        Result = $true
+    }
+    $part2['gameid'] = $gameid
+    $_ -replace 'Game .+?: (.+)','$1' -split ';' -replace '^\s+|\s+$' | % {
+    $_ -split ',' -replace '^\s+|\s+$' | % {
+        [int]$amount,$color = $_ -split ' '
+        if ($part2[$color] -lt $amount) {$part2[$color] = $amount}
+        if ($amount -gt $limits[$color]) {
+            $result.result = $false
+        }
+    }
+}
+$result
+$part2['pow'] = $part2['red']*$part2['green']*$part2['blue']
+$part2result.add([pscustomobject]$part2)
+}
+
+[pscustomobject]@{
+    Part1 = [int]$($sumofids| ? result -eq $true | select -expand game | measure-object -sum | % sum)
+    Part2 = [int]$($part2result.pow | measure-object -sum | % sum)
+}
