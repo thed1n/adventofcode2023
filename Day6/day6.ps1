f6e27fb02e6e5113cdd4accf576754eec70683ba
thed1n <tomas.hedin@gmail.com>
2023-12-07 17:05:48 +0100

    day6 update

--- Day6/day6.ps1
+++ Day6/day6.ps1
@@ -3,12 +3,12 @@ $data = Get-Content .\Day6\input.txt
 
 $raceinfo = @{}
 $race2 = @{}
-$data | % {
-    $label,$times = $_ -split ':'
+$data | ForEach-Object {
+    $label, $times = $_ -split ':'
     [int[]]$timesint = $times.trim() -split '\s+'
     [int64]$race2time = $times -replace '\s+'
-    $race2.add($label,$race2time)
-    $raceinfo.add($label,$timesint)
+    $race2.add($label, $race2time)
+    $raceinfo.add($label, $timesint)
 }
 
 $raceinfo = [pscustomobject]$raceinfo
@@ -25,79 +25,48 @@ function get-numways {
         [switch]$first,
         [switch]$backward
     )
-    
-    $movespeed = $press*1
-    $racetime = [math]::abs($press - $time)
-    if ($press -gt $time) {return}
-    write-verbose "Press: [$press], Movespeed: [$movespeed], Racetime: [$racetime], Distance: [$distance]"
-    if ($movespeed*$racetime -gt $distance) {
-        
-        if($first) {
-            return $press
-        }
-        else {1}
-    }
-    if ($backward) {
-        
-        $PSBoundParameters['press']--
-        get-numways @PSBoundParameters
-
-    } else {
-    $PSBoundParameters['press']++
-    get-numways @PSBoundParameters
-    }
-}
-
-function get-numwayspt2 {
-    [cmdletbinding()]
-    param(
-        [int64]$time,
-        [int64]$distance,
-        [int64]$press,
-        [switch]$first,
-        [switch]$backward
-    )
     begin {
         [stack[int64]]$stack = @($press)
     }
     process {
         while ($stack.count -gt 0) {
             $press = $stack.pop()
-            $movespeed = $press*1
             $racetime = [math]::abs($press - $time)
-            if ($press -gt $time) {return}
-            write-verbose "Press: [$press], Movespeed: [$movespeed], Racetime: [$racetime], Distance: [$distance]"
-            if ($movespeed*$racetime -gt $distance) {
+            if ($press -gt $time) { return }
+            Write-Verbose "Press: [$press], Racetime: [$racetime], Distance: [$distance]"
+            if ($press * $racetime -gt $distance) {
                 
-                if($first) {
+                if ($first) {
                     return $press
                 }
-                else {1}
+                else { 1 }
             }
             if ($backward) {
-                
                 $press--
                 $stack.Push($press)
-
-            } else {
+            }
+            else {
                 $press++
                 $stack.Push($press)
             }
-            }
+        }
     }
 }
 
-$result = for ($i=0; $i -lt $raceinfo.time.count; $i++){
-    get-numways -time $raceinfo.time[$i] -distance $raceinfo.distance[$i] -press 0 -Verbose | Measure-Object -sum | % sum
+$result = for ($i = 0; $i -lt $raceinfo.time.count; $i++) {
+    get-numways -time $raceinfo.time[$i] -distance $raceinfo.distance[$i] -press 0 | Measure-Object -Sum | ForEach-Object sum
 }
 $sum = 1
-$result | % {
+$result | ForEach-Object {
     $sum *= $_
 }
-$sum
 
-$first = get-numwayspt2 -time $raceinfo2.time -distance $raceinfo2.distance -press 0 -first
-$last = get-numwayspt2 -time $raceinfo2.time -distance $raceinfo2.distance -press $raceinfo2.time -first -backward
+$first = get-numways -time $raceinfo2.time -distance $raceinfo2.distance -press 0 -first
+$last = get-numways -time $raceinfo2.time -distance $raceinfo2.distance -press $raceinfo2.time -first -backward
 
-$sum2 = $last -$first+1
-$sum2
+$sum2 = $last - $first + 1
+
+[pscustomobject]@{
+    Part1 = $sum
+    Part2 = $sum2
+}
