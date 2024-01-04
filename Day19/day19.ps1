1fcc4f44a4eca3432132356c676d5afbff855b30
thed1n <tomas.hedin@gmail.com>
2024-01-04 13:29:40 +0100

    Day 19: Aplenty

---
+++ Day19/day19.ps1
@@ -0,0 +1,62 @@
+using namespace System.Collections.Generic
+$data = Get-Content .\Day19\input.txt
+
+$functiondef = [list[string]]@()
+$inputdef = [list[string]]@()
+$data | ForEach-Object {
+    if ([string]::IsNullOrWhiteSpace($_)) {
+        return
+    }
+
+    if ($_ -match '^\{') {
+        $inputdef.add($_)
+    } 
+    else { $functiondef.add($_) }
+}
+
+$functiondef | ForEach-Object {
+    $pos = $_.indexof('{')
+    $name = $_.substring(0, $pos)
+
+    $commandstring = 'param([int]$x,[int]$m,[int]$a,[int]$s)
+'
+    $commandstring += ($_ -replace '^.+{(.+)}$', '$1' -split ',') | ForEach-Object {
+
+        if ($_ -notmatch '[<>:]') {
+            "return '$_'"
+        }
+        else {
+            $result = $_ -replace '^.+:(.+)', '$1'
+            $commandline = $_ -replace '^(\w{1})', '$1' -replace '>', ' -gt ' -replace '<', ' -lt ' -replace ':(.+)$'
+
+            'if (${0}) {{return "{1}"}};' -f $commandline, $result
+        }
+    }
+    New-Item -Path function: -Name "get-$name" -Value $commandstring -Force
+}
+
+[int]$sum = 0
+$inputdef | % {
+    [int]$x,[int]$m,[int]$a,[int]$s = $_ -replace '{|}' -split ',' -replace '\w='
+
+$result = [list[string]]@()
+
+$start = 'in'
+while ($true) {
+    if ($start -eq 'A') {
+        $result.add($start)
+        $sum+= ($x+$m+$a+$s)
+        break
+    }
+    if ($start -eq 'R' ) {
+        $result.add($start)
+        break
+    }
+    $result.add($start)
+    #write-host "$start -x $x -m $m -a $a -s $s"
+    $start = Invoke-Expression "get-$start -x $x -m $m -a $a -s $s"
+}
+#$result -join ' -> '
+
+}
+$sum
